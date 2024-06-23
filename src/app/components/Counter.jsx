9f293b38017a4834885ecba7b158206020b551ce
Vitaly K <43697710+shut56@users.noreply.github.com>
2024-06-23 13:06:34 +0500

    Fix homework (#1)
    * - Fix counter
    - Fix reset button style
    
    * Fix homework number

--- src/app/components/Counter.jsx
+++ src/app/components/Counter.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useLayoutEffect, useState } from 'react';
 
 import './styles.css';
 
@@ -7,69 +7,82 @@ import './styles.css';
  * 1) Значение каунтера не должно быть отрицательным.
  * 2) Добавить кнопку обнуления каунтера.
  * 3) Добавить кнопки изменения значения на +10 и -10.
- * 4) При достижении значения больше 50, кнопка "Больше" должна заблокироваться.
+ * 4) При достижении значения 50, кнопка "Больше" должна заблокироваться.
  * 5) Значение каунтера не должно никогда превышать 50.
  */
 
 export const Counter = () => {
   const [count, setCount] = useState(0);
+  const [isDisabledInc, setIsDisabledInc] = useState(false);
+  const [isDisabledDec, setIsDisabledDec] = useState(false);
 
   const inc = useCallback(() => {
     setCount((prev) => {
-      if (prev >= 49) return prev = 50
-      else {
-        return prev + 1
+      if (prev >= 49) {
+        return 50;
       }
+
+      return prev + 1;
     });
   }, []);
 
   const minusTen = useCallback(() => {
     setCount((prev) => {
-      if (prev <= 10) return prev = 0
-      else {
-        return prev - 10
+      if (prev <= 10) {
+        return 0;
       }
+
+      return prev - 10;
     });
   }, []);
 
   const plusTen = useCallback(() => {
     setCount((prev) => {
-      if (prev >= 40) return prev = 50
-      else {
-        return prev + 10
+      if (prev >= 40) {
+        return 50;
       }
+
+      return prev + 10;
     });
   }, []);
 
   const dec = useCallback(() => {
     setCount((prev) => {
-      if (prev === 0) return prev = 0
-      else {
-        return prev - 1
+      if (prev === 0) {
+        return 0;
       }
+
+      return prev - 1;
     });
   }, []);
 
   const reset = useCallback(() => {
-    setCount(0)
-  })
+    setCount(0);
+  }, []);
+
+  useLayoutEffect(() => {
+    setIsDisabledDec(count <= 0);
+    setIsDisabledInc(count >= 50);
+  }, [count]);
 
   return (
-    <div className="counter">
-      <button type="button" onClick={minusTen}>
-        -10
-      </button>
-      <button type="button" onClick={dec}>
-        -1
-      </button>
-      <div className="count">{count}</div>
-      <button type="button" onClick={inc}>
-        +1
-      </button>
-      <button type="button" onClick={plusTen}>
-        +10
-      </button>
-      <button className="reset" type="button" onClick={reset}>
+    <div className="counter-wrapper">
+      <div className="counter">
+        <button type="button" onClick={minusTen} disabled={isDisabledDec}>
+          -10
+        </button>
+        <button type="button" onClick={dec} disabled={isDisabledDec}>
+          -1
+        </button>
+        <div className="count">{count}</div>
+        <button type="button" onClick={inc} disabled={isDisabledInc}>
+          +1
+        </button>
+        <button type="button" onClick={plusTen} disabled={isDisabledInc}>
+          +10
+        </button>
+      </div>
+      <button type="button" onClick={reset} disabled={isDisabledDec}>
         0
       </button>
     </div>
