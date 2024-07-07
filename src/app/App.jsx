c9a271cadc4efd233d97ed237705df69e88db1fd
Salpan <128787929+Salpan@users.noreply.github.com>
2024-07-07 13:10:00 +0500

    Feature/finished my homework (#2)
    * Added User List and reload button
    
    * Add gallery and new Loader component
    
    * Add router and links
    
    * Add main and make App more clear
    
    * Add feed, take posts and make button
    
    * Created functionality for posts and sort button
    
    * Bugfix/added user list (#3)
    
    * Fix footer style
    
    * Fix navigation and feed styles
    
    * Fix feed requests
    
    * Add prettier
    
    ---------
    
    Co-authored-by: Vitaly K <43697710+shut56@users.noreply.github.com>

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,19 +1,28 @@
-import { Counter } from './components/Counter';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Counter } from "./components/counter/Counter";
+import Gallery from "./components/gallery/Gallery";
+import UserList from "./components/user-list/UserList";
 
-import './styles.css';
+import { Layout } from "../common/components/layout/Layout";
+import { Feed } from "./components/feed/Feed";
+import { Main } from "./components/main/Main";
 
 const App = () => {
-  return (
-    <div className="wrapper">
-      <header>
-        <h1>Simply counter</h1>
-      </header>
-      <main>
-        <Counter />
-      </main>
-      <footer>8 800 555 35-35</footer>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Layout>
+                <Routes>
+                    <Route path="/*" element={<Main />}>
+                        <Route element={<Gallery />} index={true} />
+                        <Route path="gallery" element={<Gallery />} />
+                        <Route path="userlist" element={<UserList />} />
+                        <Route path="counter" element={<Counter />} />
+                        <Route path="posts" element={<Feed />} />
+                    </Route>
+                </Routes>
+            </Layout>
+        </BrowserRouter>
+    );
 };
 
 export default App;
---
+++ src/app/components/feed/Feed.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import "./styles.css";
+
+const url = "https://jsonplaceholder.typicode.com/posts";
+
+export const Feed = () => {
+    const [post, setPost] = useState([]);
+    const [isClick, setIsClick] = useState(true);
+
+    useEffect(() => {
+        fetch(url)
+            .then((r) => r.json())
+            .then((data) => {
+                setPost(data);
+            });
+    }, []);
+
+    const onClick = () => {
+        setIsClick((prev) => !prev);
+        setPost((prev) => prev.reverse());
+    };
+
+    return (
+        <>
+            <button
+                className={isClick === true ? "sort" : "noSort"}
+                type="button"
+                onClick={onClick}
+            >
+                {isClick === true ? `↑` : `↓`}
+            </button>
+            <ul>
+                {post.map((post) => {
+                    return (
+                        <li key={post.id}>
+                            <p>ID: {post.id}</p>
+                            <p className="paragraph">
+                                {post.title.toUpperCase()}
+                            </p>
+                            {post.body[0].toUpperCase() + post.body.slice(1)}
+                        </li>
+                    );
+                })}
+            </ul>
+        </>
+    );
+};
