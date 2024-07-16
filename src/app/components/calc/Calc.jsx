3d5cccba402f684f1b269761bd55dd8ed4c9a42b
Salpan <rus2009ian@gmail.com>
2024-07-16 15:14:04 +0500

    Deleted data.js

--- src/app/components/calc/Calc.jsx
+++ src/app/components/calc/Calc.jsx
@@ -1,34 +1,3 @@
-/**
- * Задание: Написать калькулятор
- * 1) Калькулятор должен содержать 10 кнопок с цифрами от 0 до 9.
- * 2) Добавить кнопки сложения, вычитания, умножения, деления.
- * 3) Добавить кнопки равенства, сброса.
- * 4) Кнопки должны отрисовываться динамически, на основе массива.
- * 5) Добавить input, в который будет выводиться промежуточное значение.
- */
-
-// Example: https://github.com/shut56/chat/blob/master/client/components/common/message-input.js
-
-// [
-//     {
-//         label: "0",
-//         value: 0,
-//         type: "numeric",
-//     },
-//     {
-//         label: "=",
-//         value: null,
-//         type: "functionality",
-//     },
-// ];
-
-// Получение значения инпута:
-//
-// const onChangeHandler = (e) => {
-//     setInputValue(e.target.value)
-// }
-//
-// <input onChange={onChangeHandler} />
 
 import { useCallback, useEffect, useMemo, useState } from 'react';
 import { arrayCalc } from './consts';
