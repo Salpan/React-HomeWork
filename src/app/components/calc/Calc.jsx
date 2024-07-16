0d2e5b6dac8b23e9ea40466fe9de16557102b476
Vitaly K <43697710+shut56@users.noreply.github.com>
2024-07-16 14:39:16 +0500

    Bugfix/calc (#5)
    * Update calculate logic
    
    * Fix some issues
    
    ---------
    
    Co-authored-by: Salpan <rus2009ian@gmail.com>

--- src/app/components/calc/Calc.jsx
+++ src/app/components/calc/Calc.jsx
@@ -30,117 +30,192 @@
 //
 // <input onChange={onChangeHandler} />
 
-import { useEffect, useState } from "react";
-import { arrayCalc } from "./consts/data";
-import "./styles.css";
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import { arrayCalc } from './consts';
+import './styles.css';
 
 export const Calc = () => {
-    const [prev, setPrev] = useState("0");
-    const [curr, setCurr] = useState("0");
-    const [input, setInput] = useState("0");
+    const [prev, setPrev] = useState('0');
+    const [curr, setCurr] = useState('0');
     const [operator, setOperator] = useState(null);
     const [total, setTotal] = useState(false);
-    // const [inputValue, setInputValue] = useState('')
+    const [errorText, setErrorText] = useState('');
 
-    // const onChangeHandler = (e) => {
-    //     setInputValue(e.target.value)
-    // }
+    const onChangeHandler = useCallback((e) => {
+        setCurr(e.target.value);
+    }, []);
 
-    const numbers = (label) => {
-        if (total) {
-            setPrev("");
-        }
-        curr ? setCurr((prev) => prev + label) : setCurr(label);
-        setTotal(false);
-    };
+    const addNewDigit = useCallback(
+        (value) => {
+            if (total) {
+                setPrev('0');
+            }
 
-    useEffect(() => {
-        setInput(curr);
-    }, [curr]);
+            if (curr && curr !== '0') {
+                setCurr((prevValue) => {
+                    if (operator && prev === curr) {
+                        return String(value);
+                    }
 
-    const operators = (label) => {
-        setTotal(false);
-        setOperator(label);
-        if (curr === "") return true;
-        else {
-            if (prev !== "") {
-                equals();
+                    return String(prevValue) + String(value);
+                });
+                setTotal(false);
+            } else {
+                setCurr(String(value));
+            }
+
+            setTotal(false);
+        },
+        [curr, prev, total, operator],
+    );
+
+    const equals = useCallback(
+        (label) => {
+            if (label === '=') {
+                setTotal(true);
+            }
+
+            let calculatedValue = 0;
+
+            switch (operator) {
+                case '/':
+                    calculatedValue = parseInt(prev) / parseInt(curr);
+                    break;
+                case '*':
+                    calculatedValue = parseInt(prev) * parseInt(curr);
+                    break;
+                case '+':
+                    calculatedValue = parseInt(prev) + parseInt(curr);
+                    break;
+                case '-':
+                    calculatedValue = parseInt(prev) - parseInt(curr);
+                    break;
+                case '=': {
+                    calculatedValue = curr;
+                    break;
+                }
+                default:
+                    return;
+            }
+
+            setPrev(String(calculatedValue));
+            setCurr(String(calculatedValue));
+        },
+        [curr, operator, prev],
+    );
+
+    const operators = useCallback(
+        (label) => {
+            setTotal(false);
+            setOperator(label);
+
+            if (!curr) {
+                return;
+            }
+
+            if (prev !== '0' && curr !== prev) {
+                equals(label);
             } else {
                 setPrev(curr);
-                setCurr("");
             }
-        }
-    };
+        },
+        [curr, prev, equals],
+    );
 
-    const equals = (label) => {
-        if (label === "=") {
-            setTotal(true);
-        }
+    const reset = useCallback(() => {
+        setPrev('0');
+        setCurr('0');
+        setOperator(null);
+    }, []);
 
-        let cal = 0;
-        switch (operator) {
-            case "/":
-                cal = parseInt(prev) / parseInt(curr);
-                break;
-            case "*":
-                cal = parseInt(prev) * parseInt(curr);
-                break;
-            case "+":
-                cal = parseInt(prev) + parseInt(curr);
-                break;
-            case "-":
-                cal = parseInt(prev) - parseInt(curr);
-                break;
-            default:
-                return true;
-        }
-        setInput("");
-        setPrev(cal);
-        setCurr("");
-    };
+    const onClickHandler = useCallback(
+        (item) => {
+            return () => {
+                switch (item.type) {
+                    case 'function': {
+                        operators(item.label);
+                        break;
+                    }
+                    case 'numeric': {
+                        addNewDigit(item.value);
+                        break;
+                    }
+                    case 'equal': {
+                        equals(item.label);
+                        break;
+                    }
+                    default: {
+                        reset();
+                    }
+                }
+            };
+        },
+        [operators, addNewDigit, equals, reset],
+    );
 
-    const reset = () => {
-        setPrev("");
-        setCurr("");
-        setInput("0");
-    };
+    const buttonClassName = useMemo(
+        () => (type) => {
+            switch (type) {
+                case 'reset': {
+                    return 'reset-button';
+                }
+                case 'function': {
+                    return 'function-button';
+                }
+                case 'equal': {
+                    return 'equal-button';
+                }
+                default: {
+                    return 'num-button';
+                }
+            }
+        },
+        [],
+    );
+
+    useEffect(() => {
+        const max = Number.MAX_SAFE_INTEGER;
+        if (prev >= max || curr >= max) {
+            setErrorText('More then safe integer');
+        } else {
+            setErrorText('');
+        }
+    }, [prev, curr]);
 
     return (
         <div className="container">
-            <div className="calc">
-                <div className="input">
-                    {input !== "" || input === "0" ? input : prev}
-                </div>
-                {/* <input onChange={onChangeHandler} /> */}
-                <div className="calc-buttons">
-                    {arrayCalc.map((item, index) => {
-                        return (
-                            <button
-                                key={index}
-                                className={
-                                    item.type === "reset"
-                                        ? "reset-button"
-                                        : item.type === "function"
-                                          ? "function-button"
-                                          : item.type === "equal"
-                                            ? "equal-button"
-                                            : "num-button"
-                                }
-                                type="button"
-                                onClick={() => {
-                                    if (item.type === "function") {
-                                        operators(item.label);
-                                    } else if (item.type === "numeric") {
-                                        numbers(item.label);
-                                    } else if (item.type === "equal") {
-                                        equals(item.label);
-                                    } else reset();
-                                }}
-                            >
-                                {item.label}
-                            </button>
-                        );
-                    })}
+            <div className="calc-wrapper">
+                <div className="calc">
+                    <div className="operation-view">
+                        {prev !== '0' && <span>{prev}</span>}
+                        {operator && <span> {operator}</span>}
+                    </div>
+                    <input
+                        type="number"
+                        value={curr}
+                        onChange={onChangeHandler}
+                        className="input"
+                    />
+                    {errorText && (
+                        <span style={{ color: 'red' }}>{errorText}</span>
+                    )}
+                    <div className="calc-buttons">
+                        {arrayCalc.map((item, index) => {
+                            return (
+                                <button
+                                    key={index}
+                                    className={`base-button ${buttonClassName(item.type)}`}
+                                    type="button"
+                                    onClick={onClickHandler(item)}
+                                    disabled={
+                                        item.type !== 'reset' && errorText
+                                    }
+                                >
+                                    {item.label}
+                                </button>
+                            );
+                        })}
+                    </div>
                 </div>
             </div>
         </div>
---
+++ src/app/components/calc/consts/index.js
@@ -0,0 +1,76 @@
+export const arrayCalc = [
+    {
+        label: '7',
+        value: 7,
+        type: 'numeric',
+    },
+    {
+        label: '8',
+        value: 8,
+        type: 'numeric',
+    },
+    {
+        label: '9',
+        value: 9,
+        type: 'numeric',
+    },
+    {
+        label: 'AC',
+        type: 'reset',
+    },
+    {
+        label: '4',
+        value: 4,
+        type: 'numeric',
+    },
+    {
+        label: '5',
+        value: 5,
+        type: 'numeric',
+    },
+    {
+        label: '6',
+        value: 6,
+        type: 'numeric',
+    },
+    {
+        label: '/',
+        type: 'function',
+    },
+    {
+        label: '1',
+        value: 1,
+        type: 'numeric',
+    },
+    {
+        label: '2',
+        value: 2,
+        type: 'numeric',
+    },
+    {
+        label: '3',
+        value: 3,
+        type: 'numeric',
+    },
+    {
+        label: '*',
+        type: 'function',
+    },
+    {
+        label: '0',
+        value: 0,
+        type: 'numeric',
+    },
+    {
+        label: '+',
+        type: 'function',
+    },
+    {
+        label: '-',
+        type: 'function',
+    },
+    {
+        label: '=',
+        type: 'equal',
+    },
+];
