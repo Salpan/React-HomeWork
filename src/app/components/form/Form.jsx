5a3af88b60bd4af7c34cb5ff56591b1a57b48b59
Salpan <rus2009ian@gmail.com>
2024-08-01 20:16:53 +0500

    Add sort for cities and some styles

--- src/app/components/form/Form.jsx
+++ src/app/components/form/Form.jsx
@@ -2,31 +2,40 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import { useEffect, useState } from 'react';
 import MenuItem from '@mui/material/MenuItem';
+import './styles.css'
 
 export const Form = () => {
 
     const url = "https://raw.githubusercontent.com/arbaev/russia-cities/master/russia-cities.json"
 
-    const [city, setCity] = useState([])
+    const [cities, setCity] = useState([])
 
     useEffect(() => {
         fetch(url)
             .then((r) => r.json())
             .then((data) => {
-                setCity((data));
+                setCity((data.sort((a, b) => a.name > b.name ? 1 : -1)));
             })
     }, []);
 
-    const citysName = city.map((city) => city.name).sort()
+    const citysName = cities.map((city) => city.name).sort()
 
     console.log(citysName)
 
     return (
-        <>
+        <div className='form-box'>
             <Box
                 component="form"
                 sx={{
                     '& .MuiTextField-root': { m: 1, width: "250px" },
+                    display: "flex",
+                    alignContent: "center",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    flexWrap: "100%",
+                    border: "1px solid",
+                    borderRadius: "4px",
+                    width: "600px"
                 }}
                 noValidate
                 autoComplete="off"
@@ -45,27 +54,25 @@ export const Form = () => {
                 </ div>
                 <div>
                     <TextField
-                        id="outlined-multiline-static"
-                        label="О себе"
-                        multiline
-                        rows={4}
-                    />
-                    <TextField
                         id="outlined-select-currency"
                         select
                         label="Ваш город"
                         defaultValue=""
                     >
-                        {city.map((city) => {
-                            return (
-                                <MenuItem key={city.id} >
-                                    {city.name}
-                                </MenuItem>
-                            )
-                        })}
+                        {cities.map((city) =>
+                            <MenuItem key={city.id} value={city.name}>
+                                {city.name}
+                            </MenuItem>
+                        )}
                     </TextField>
+                    <TextField
+                        id="outlined-multiline-static"
+                        label="О себе"
+                        multiline
+                        rows={4}
+                    />
                 </div>
             </Box>
-        </>
+        </ div>
     )
 }
