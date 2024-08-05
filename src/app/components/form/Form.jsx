b91b9cae923cb15a103594dc78655778c7e2ba59
Salpan <rus2009ian@gmail.com>
2024-08-05 20:23:27 +0500

    Add checkbox

--- src/app/components/form/Form.jsx
+++ src/app/components/form/Form.jsx
@@ -2,8 +2,9 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import { useEffect, useState } from 'react';
 import MenuItem from '@mui/material/MenuItem';
+import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel from '@mui/material/FormControlLabel';
 import './styles.css'
-import { Button } from '@mui/material';
 
 export const Form = () => {
 
@@ -19,66 +20,70 @@ export const Form = () => {
             })
     }, []);
 
-    const citysName = cities.map((city) => city.name).sort()
-
-    console.log(citysName)
-
     return (
         <div className='form-box'>
             <p className='registration'>
                 Регистрация на Tiffany.ru
             </p>
             <Box
-                component="form"
+                component='form'
                 sx={{
-                    '& .MuiTextField-root': { m: 2, width: "260px" },
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                    border: "1px solid",
-                    borderRadius: "4px",
-                    width: "600px",
+                    '& .MuiTextField-root': { m: 2, width: '260px' },
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    border: '1px solid ',
+                    borderRadius: '4px',
+                    width: '600px',
                 }}
                 noValidate
-                autoComplete="off"
+                autoComplete='off'
             >
                 <div>
                     <TextField
                         required
-                        id="outlined-required"
-                        label="Фамилия"
+                        id='outlined-required'
+                        label='Фамилия'
                     />
                     <TextField
                         required
-                        id="outlined-required"
-                        label="Имя"
+                        id='outlined-required'
+                        label='Имя'
                     />
                 </ div>
-                <div>
+                <div className='div-cullom'>
+                    <div className='div-wrapper'>
+                        <TextField
+                            id='outlined-select-currency'
+                            select
+                            label='Ваш город'
+                            defaultValue=''
+                        >
+                            {cities.map((city) =>
+                                <MenuItem key={city.id} value={city.name}>
+                                    {city.name}
+                                </MenuItem>
+                            )}
+                        </TextField>
+                        <FormControlLabel
+                            value="start"
+                            control={<Checkbox />}
+                            label="Я от Сенса"
+                            labelPlacement="start"
+                        />
+                    </div>
                     <TextField
-                        id="outlined-select-currency"
-                        select
-                        label="Ваш город"
-                        defaultValue=""
-                    >
-                        {cities.map((city) =>
-                            <MenuItem key={city.id} value={city.name}>
-                                {city.name}
-                            </MenuItem>
-                        )}
-                    </TextField>
-                    <TextField
-                        id="outlined-multiline-static"
-                        label="О себе"
+                        id='outlined-multiline-static'
+                        label='О себе'
                         multiline
                         rows={4}
                     />
                 </div>
-                <div className='submit-btn'>
+                <div className='div-wrapper'>
                     <TextField
-                        sx={{ backgroundColor: '#5dccf8' }}
+                        sx={{ backgroundColor: '#5dccf8', borderRadius: '5px' }}
                         type='submit'
-                        id="outlined-multiline-static"
+                        id='outlined-multiline-static'
                         value='Регистрация'
                         rows={4}
                     />
