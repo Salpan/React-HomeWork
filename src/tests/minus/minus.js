48217e154e084dff1a5bcad94792ebd9582884ed
Salpan <rus2009ian@gmail.com>
2024-09-26 17:19:08 +0500

    Add two tests with sum and minus

---
+++ src/tests/minus/minus.js
@@ -0,0 +1,6 @@
+
+export { minus }
+
+const minus = (a, b) => {
+    return a - b
+}
---
+++ src/tests/minus/minus.test.js
@@ -0,0 +1,5 @@
+import { minus } from "./minus"
+
+test('adds 2 - 1 to equel 1', () => {
+    expect(minus(2, 1)).toBe(1)
+})
